fc3b146fe8e447eae31c8115919387f495508b7e
xiaoke@ivgsz_171 <xiaokehuang@foxmail.com>
2023-03-03 16:55:02 +0800

    fix result dir

--- scripts/generate_eval_sh.py
+++ scripts/generate_eval_sh.py
@@ -18,11 +18,11 @@ for _time in [5*60, 10*60, 60*60*2]:
         if int(j) > 300:
             weights_dir = f"{base_dir}/zju_mocap/{_time}/test_{j}/"
             weight_epoch = find_largest_epoch(weights_dir)
-            print(f'id={j}; python run.py --type evaluate --cfg_file configs/zju_mocap_exp/multi_view_${{id}}.yaml exp_name zju_mocap/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} cfg.result_dir data/result/novel_view &')
+            print(f'id={j}; python run.py --type evaluate --cfg_file configs/zju_mocap_exp/multi_view_${{id}}.yaml exp_name zju_mocap/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} result_dir data/result/novel_view &')
         else:
             weights_dir = f"{base_dir}/h36m/{_time}/test_{j}/"
             weight_epoch = find_largest_epoch(weights_dir)
-            print(f'id={j}; python run.py --type evaluate --cfg_file configs/h36m_exp/latent_xyzc_s${{id}}p.yaml exp_name h36m/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} cfg.result_dir data/result/novel_view &')
+            print(f'id={j}; python run.py --type evaluate --cfg_file configs/h36m_exp/latent_xyzc_s${{id}}p.yaml exp_name h36m/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} result_dir data/result/novel_view &')
     print('wait')
 
 for _time in [5*60, 10*60, 60*60*2]:
@@ -33,9 +33,9 @@ for _time in [5*60, 10*60, 60*60*2]:
         if int(j) > 300:
             weights_dir = f"{base_dir}/zju_mocap/{_time}/test_{j}/"
             weight_epoch = find_largest_epoch(weights_dir)
-            print(f'id={j}; python run.py --type evaluate --cfg_file configs/zju_mocap_exp/multi_view_${{id}}.yaml exp_name zju_mocap/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} cfg.result_dir data/result/novel_view test_novel_pose True &')
+            print(f'id={j}; python run.py --type evaluate --cfg_file configs/zju_mocap_exp/multi_view_${{id}}.yaml exp_name zju_mocap/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} result_dir data/result/novel_pose test_novel_pose True &')
         else:
             weights_dir = f"{base_dir}/h36m/{_time}/test_{j}/"
             weight_epoch = find_largest_epoch(weights_dir)
-            print(f'id={j}; python run.py --type evaluate --cfg_file configs/h36m_exp/latent_xyzc_s${{id}}p.yaml exp_name h36m/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} cfg.result_dir data/result/novel_view test_novel_pose True &')
+            print(f'id={j}; python run.py --type evaluate --cfg_file configs/h36m_exp/latent_xyzc_s${{id}}p.yaml exp_name h36m/{_time}/test_${{id}} gpus [{i%num_gpus}] test.epoch {weight_epoch} result_dir data/result/novel_pose test_novel_pose True &')
     print('wait')
